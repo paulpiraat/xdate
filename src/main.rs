272a6715fe10092af3528388e1dfe7a40d7cf956
Paul Boelens <info@paulboelens.com>
2018-08-12 14:06:56 +0200

    Only use in X mode

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 extern crate chrono;
 extern crate clap;
+extern crate xcb;
 
-use std::{thread, time};
 use chrono::prelude::*;
-use clap::{Arg, App};
+use clap::{App, Arg};
+use std::{thread, time};
 
 fn main() {
+
+    if !is_connected() {
+        println!("Could not connect to X!");
+        return;
+    }
+
     let matches = App::new("xClock")
-        .arg(Arg::with_name("format")
-             .short("f")
-             .long("format")
-             .help("Use the given `printf` style format")
-             .takes_value(true))
-        .arg(Arg::with_name("snoop")
-             .short("s")
-             .long("snoop")
-             .help("Active the snoop mode")
-             .takes_value(false))
+        .arg(
+            Arg::with_name("format")
+                .short("f")
+                .long("format")
+                .help("Use the given `printf` style format")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("snoop")
+                .short("s")
+                .long("snoop")
+                .help("Active the snoop mode")
+                .takes_value(false),
+        )
         .get_matches();
 
     let format = matches.value_of("format").unwrap_or("");
@@ -26,16 +37,30 @@ fn main() {
         let wait_time = time::Duration::from_millis(1000);
 
         loop {
-            thread::sleep(wait_time);
+            if !is_connected() {
+                break;
+            }
+
             print_date(format);
+            thread::sleep(wait_time);
         }
     } else {
         print_date(format);
     }
 }
 
+fn is_connected() -> bool {
+    let entries = xcb::Connection::connect(None);
+    let mut has_connection = false;
+    for _entry in entries {
+        has_connection = true;
+        break;
+    }
+    return has_connection
+}
+
 fn print_date(format: &str) {
     let local: DateTime<Local> = Local::now();
     let dt = local.format("%a %e %b %T").to_string();
-    println!("{}{}", format, dt); 
+    println!("{}{}", format, dt);
 }
